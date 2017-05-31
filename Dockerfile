12b3cd63a4384d80b1a118656073759851b1bb3a
Paweł Gesek <pgesek@soldevelo.com>
2017-05-31 18:01:53 +0200

    #101: Made nginx proxy to dhis & empi using SSL

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,7 @@
 FROM nginx:1.13-alpine
 
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY utils/replace-vars /replace-vars
+COPY run.sh /run.sh
+
+CMD ./run.sh
---
+++ run.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+: ${IL_TRUSTCHAIN=""}
+: ${IL_CONSOLE_CERT=/etc/nginx/certs/sedish.net.crt}
+: ${IL_CONSOLE_KEY=/etc/nginx/certs/sedish.net.key}
+: ${SERVER_NAME=localhost}
+: ${DHIS_FWD_PORT=9001}
+: ${OPENEMPI_FWD_PORT=9002}
+
+export IL_CONSOLE_CERT
+export IL_CONSOLE_KEY
+export IL_TRUSTCHAIN
+export SERVER_NAME
+export DHIS_FWD_PORT
+export OPENEMPI_FWD_PORT
+
+/replace-vars /etc/nginx/nginx.conf
+
+nginx -g "daemon off;"
