777e0012d16d0e02a953aab44179877babe06ded
Paweł Gesek <pgesek@soldevelo.com>
2017-05-31 18:25:07 +0200

    #101: Added a proxy for OpenSHR

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 : ${SERVER_NAME=localhost}
 : ${DHIS_FWD_PORT=9001}
 : ${OPENEMPI_FWD_PORT=9002}
+: ${OPENSHR_FWD_PORT=9003}
 
 export IL_CONSOLE_CERT
 export IL_CONSOLE_KEY
@@ -13,6 +14,7 @@ export IL_TRUSTCHAIN
 export SERVER_NAME
 export DHIS_FWD_PORT
 export OPENEMPI_FWD_PORT
+export OPENSHR_FWD_PORT
 
 /replace-vars /etc/nginx/nginx.conf
 
