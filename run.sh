147585e38db99a833a28aecd9f78634392f3573e
Marcin Kruszyński <mkruszynski@soldevelo.com>
2017-07-17 09:58:05 +0200

    #112: Renamed IL and ILR variables

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #/bin/sh
 
-: ${IL_TRUSTCHAIN=""}
-: ${IL_CONSOLE_CERT=/etc/nginx/certs/sedish.net.crt}
-: ${IL_CONSOLE_KEY=/etc/nginx/certs/sedish.net.key}
+: ${TRUSTCHAIN=""}
+: ${CERT_LOCATION=/etc/nginx/certs/sedish.net.crt}
+: ${KEY_LOCATION=/etc/nginx/certs/sedish.net.key}
 : ${SERVER_NAME=localhost}
 : ${DHIS_FWD_PORT=9001}
 : ${OPENEMPI_FWD_PORT=9002}
 : ${OPENSHR_FWD_PORT=9003}
 
-export IL_CONSOLE_CERT
-export IL_CONSOLE_KEY
-export IL_TRUSTCHAIN
+export CERT_LOCATION
+export KEY_LOCATION
+export TRUSTCHAIN
 export SERVER_NAME
 export DHIS_FWD_PORT
 export OPENEMPI_FWD_PORT
